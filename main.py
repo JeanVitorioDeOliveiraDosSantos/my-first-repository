37513073680e774ef4c227ea1105c784156856c3
Jean Vitorio <jeanInoBran@gmail.com>
2021-10-08 17:03:20 -0300

    Adicionada funcao para printar sobrenome

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def hello():
 def print_name(name):
     print(name)
 
+def print_sobrenome(sobrenome):
+    print(sobrenome)
+
 if __name__ == '__main__':
     hello()
 
     print_name("Jean")
+    print_sobrenome("Santos")
